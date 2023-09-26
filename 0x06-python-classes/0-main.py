5f56722fd3da0c5f26e5669e74eea0bb110fff08
ezralx <ezra.teshomew@gmail.com>
2023-09-26 23:54:10 +0300

    an empty class Square that defines a square

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,3 +1,4 @@
+guillaume@ubuntu:~/0x06$ cat 0-main.py
 #!/usr/bin/python3
 Square = __import__('0-square').Square
 
---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defining a class Square"""
+
+
+class Square:
+    """Class that defines properties of square.
+    """
+    pass
