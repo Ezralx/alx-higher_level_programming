1b3105921076338e4f372cbf4a3a7020f7e13747
ezralx <ezra.teshomew@gmail.com>
2023-10-03 00:40:09 +0300

    more classes and objects all task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+ define a  Rectangle 
+"""
+
+
+class Rectangle:
+    """  empty class represent a rectangle """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
+
+
+class Rectangle:
+    """ 
+    defines a rectangle by: (based on 0-rectangle.py) 
+    Attributes: width(int) & hight(int)
+
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """ WIDTH """
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """ HEIGHT """
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
