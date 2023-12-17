789c31bc01048a633ad1beb6543c5f3644ad0dc1
Ezralx <ezrateshome.alx@gmail.com>
2023-12-17 23:30:05 +0300

    update:pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-    
+   
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                         host='localhost',port=3306)
+                         host='localhost', port=3306)
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
@@ -21,3 +21,4 @@ if __name__ == '__main__':
 
     cur.close()
     db.close()
+    
