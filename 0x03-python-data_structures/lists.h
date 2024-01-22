adf612968e80b01bb43a948c06da4ff500058ed3
Ezralx <ezrateshome.alx@gmail.com>
2024-01-22 23:27:32 +0300

    feat(task 13:a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,9 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -9,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Alx project 03 sorint 2
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -21,34 +23,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
-
-#endif /* LISTS_H */
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Alx project 03 sorint 2
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-
-int is_palindrome(listint_t **head);
-void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
