2777f7bb80ac070928046d0f09e4706bf57f2582
Ezralx <ezrateshome.alx@gmail.com>
2024-02-19 22:11:13 +0300

    feat:writing  class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry
+        Attributes: None.
+        Methods:
+            area() - raises an Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
